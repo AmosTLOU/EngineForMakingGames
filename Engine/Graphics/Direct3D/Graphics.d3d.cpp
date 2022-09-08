f73ec78c7904038e8e8ca64886f57c8a84e731bb
AmosTLOU <didala2009@163.com>
2022-09-08 01:36:46 -0600

    add a few logging messages

--- Engine/Graphics/Direct3D/Graphics.d3d.cpp
+++ Engine/Graphics/Direct3D/Graphics.d3d.cpp
@@ -258,7 +258,7 @@ eae6320::cResult eae6320::Graphics::Initialize( const sInitializationParameters&
 			return result;
 		}
 	}
-
+	Logging::OutputMessage("Assignment01 MyGame's Graphics Project finished Initialization");
 	return result;
 }
 
@@ -305,7 +305,7 @@ eae6320::cResult eae6320::Graphics::CleanUp()
 			}
 		}
 	}
-
+	Logging::OutputMessage("Assignment01 MyGame's Graphics Project finished CleanUp");
 	return result;
 }
 
--- Engine/Graphics/OpenGL/Graphics.gl.cpp
+++ Engine/Graphics/OpenGL/Graphics.gl.cpp
@@ -243,7 +243,7 @@ eae6320::cResult eae6320::Graphics::Initialize( const sInitializationParameters&
 			return result;
 		}
 	}
-
+	Logging::OutputMessage("Assignment01 MyGame's Graphics Project finished Initialization");
 	return result;
 }
 
@@ -279,7 +279,7 @@ eae6320::cResult eae6320::Graphics::CleanUp()
 			}
 		}
 	}
-
+	Logging::OutputMessage("Assignment01 MyGame's Graphics Project finished CleanUp");
 	return result;
 }
 
